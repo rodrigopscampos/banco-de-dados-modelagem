b69a02ea6b9b117be0ebcef01a1ea038eaa62ae0
Rodrigo Campos <rodigopscampos@gmail.com>
2019-09-06 18:44:25 -0300

    altera script para criar turmas com inicio < hoje e fim > hoje

--- escola/escola.sql
+++ escola/escola.sql
@@ -13,6 +13,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+CREATE DATABASE ESCOLA IF NOT EXIST;
+USE ESCOLA;
+
 --
 -- Table structure for table `aluno_turma`
 --
@@ -397,7 +400,7 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Dumping data for table `turmas`
 --
 
-INSERT INTO `turmas` (`id`, `dt_inicio`, `dt_fim`, `duracao`, `cursos_id`, `professor_id`) VALUES (1,'2019-04-07','2019-05-04','17:45:21',5,33);
+INSERT INTO `turmas` (`id`, `dt_inicio`, `dt_fim`, `duracao`, `cursos_id`, `professor_id`) VALUES (1, now() - interval 1 day , now() + interval 20 day, '17:45:21',5,33);
 INSERT INTO `turmas` (`id`, `dt_inicio`, `dt_fim`, `duracao`, `cursos_id`, `professor_id`) VALUES (2,'2019-02-05','2019-02-28','13:45:10',11,37);
 INSERT INTO `turmas` (`id`, `dt_inicio`, `dt_fim`, `duracao`, `cursos_id`, `professor_id`) VALUES (3,'2019-04-07','2019-05-15','11:08:42',2,41);
 INSERT INTO `turmas` (`id`, `dt_inicio`, `dt_fim`, `duracao`, `cursos_id`, `professor_id`) VALUES (4,'2019-02-19','2019-04-19','10:01:07',2,39);
